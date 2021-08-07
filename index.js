2399b5b4743789d3a65ee554e90cab8f94910764
Abhay Banda <bandaabhay@gmail.com>
2021-08-07 15:41:47 +0530

    compareDNA() method added and mutate() method shifted insode thee pAequor obj

--- index.js
+++ index.js
@@ -16,23 +16,36 @@ const returnRandBase = () => {
 const pAequorFactory = (organismNumber, DNABase) => {
     const specimen = {
         'specimenNum' : organismNumber,
-        'dna': DNABase
+        'dna': DNABase,
+        mutate() {
+            const newBaseIndex = Math.floor(Math.random() * this.dna.length);
+            const selectedBase = this.dna[newBaseIndex];
+            let newBase = selectedBase;
+            while(newBase === selectedBase) {
+                newBase = returnRandBase();
+            }
+            this.dna[newBaseIndex] = newBase;
+
+            return this.dna;
+        },
+        compareDNA(pAequorObj) {
+            let commonDNACounter = 0;
+            for(let i = 0; i<15; ++i) {
+                const pAequorBase = pAequorObj.dna[i];
+                const currentpAequorBase = this.dna[i];
+                commonDNACounter += pAequorBase === currentpAequorBase ? 1 : 0;
+            }
+            return ((commonDNACounter/15)*100).toFixed(2);
+        }
+        
     };
     return specimen;
 }
 
-let pAequor = pAequorFactory(1,mockUpStrand())
-
-pAequor.mutate = function() {
-    const newBaseIndex = Math.floor(Math.random() * this.dna.length);
-    const selectedBase = this.dna[newBaseIndex];
-    let newBase = selectedBase;
-    while(newBase === selectedBase) {
-        newBase = returnRandBase();
-    }
-    this.dna[newBaseIndex] = newBase;
-
-    return this.dna;
-}
+let pAequor1 = pAequorFactory(1,mockUpStrand())
+let pAequor2 = pAequorFactory(2, mockUpStrand());
+console.log(pAequor1.dna);
+console.log(pAequor2.dna);
+console.log(pAequor1.compareDNA(pAequor2) + "% similarity");
 
 
